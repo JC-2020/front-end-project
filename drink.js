66294293434cdd80c4fe08ae5f7713bca6ba62a5
Wai Ki Cheung <jacky@Wais-MacBook-Air.local>
2020-09-25 12:58:09 -0500

    jackycheung-update

--- drink.js
+++ drink.js
@@ -51,3 +51,26 @@ btn1.addEventListener('click', function(event){
         })
     
 })
+
+
+$(document).ready(function() {
+    $('.pour') //Pour Me Another Drink, Bartender!
+      .delay(2000)
+      .animate({
+        height: '360px'
+        }, 1500)
+      .delay(1600)
+      .slideUp(500);
+    
+    $('#liquid') // I Said Fill 'Er Up!
+      .delay(3400)
+      .animate({
+        height: '170px'
+      }, 2500);
+    
+    $('.beer-foam') // Keep that Foam Rollin' Toward the Top! Yahooo!
+      .delay(3400)
+      .animate({
+        bottom: '200px'
+        }, 2500);
+    });
